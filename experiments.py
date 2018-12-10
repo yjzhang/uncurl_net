81a11ab150ae8239f75944b2868812b318f3dff8
yjzhang <yjzhang@cs.washington.edu>
2018-12-10 05:20:57 -0800

    fixed typo

--- experiments.py
+++ experiments.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     import scipy.io
     from purity_analysis import plot_df, build_simple_table
     data = scipy.io.mmread('../uncurl_test_datasets/10x_pure_pooled/data_8000_cells.mtx.gz')
-    actual_labels = np.loadtxt('labels_8000_cells.txt').astype(int).flatten()
+    actual_labels = np.loadtxt('../uncurl_test_datasets/10x_pure_pooled/labels_8000_cells.txt').astype(int).flatten()
     k = len(set(actual_labels))
     genes = uncurl.max_variance_genes(data, 5, 0.2)
     data_subset = data[genes, :]
